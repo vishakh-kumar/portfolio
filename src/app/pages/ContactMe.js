0a13e79c541fc7217d6e3a3d33731d0e15fa12f0
Vishakh Aswani Kumar <vishakh@vishakhs-mbp.lan>
2021-08-13 13:34:35 -0500

    wip: correction to contact me page, removing emailjs

--- src/app/pages/ContactMe.js
+++ src/app/pages/ContactMe.js
@@ -1,32 +1,15 @@
 import React from "react";
 import "../../../node_modules/bulma/bulma.sass";
 import "../../App.scss";
-import emailjs from "emailjs-com";
 
 const ContactMe = () => {
-    const sendEmail = (e) => {
-        e.preventDefault();
-
-        emailjs
-            .sendForm(
-                "service_mwks3zh",
-                "template_lj8ew5r",
-                e.target,
-                "user_zl5b822b2ELK364J9oPDL"
-            )
-            .then((res) => {
-                console.log(res);
-            })
-            .catch((err) => console.log(err));
-    };
-
     return (
         <div className="formContainer">
             <div className="contactMeIntro">
                 <h1 className="title">For further informations</h1>
             </div>
             <div className="center">
-                <form onSubmit={sendEmail}>
+                <form>
                     <label>Name:</label>
                     <input type="text" name="name" className="input" />
 
