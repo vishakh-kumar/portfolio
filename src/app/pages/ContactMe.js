874a4699b1edfdaa5df9df5e20c20c51354ab02c
Vishakh Aswani Kumar <vishakh@vishakhs-mbp.lan>
2021-08-14 12:27:30 -0500

    debugging form

--- src/app/pages/ContactMe.js
+++ src/app/pages/ContactMe.js
@@ -1,27 +1,72 @@
 import React from "react";
+import { useState } from "react";
 import "../../../node_modules/bulma/bulma.sass";
 import "../../App.scss";
 
 const ContactMe = () => {
+    const [form, setForm] = useState({ name: "", email: "", message: "" });
+    const encode = (data) => {
+        return Object.keys(data)
+            .map(
+                (key) =>
+                    encodeURIComponent(key) +
+                    "=" +
+                    encodeURIComponent(data[key])
+            )
+            .join("&");
+    };
+    const handleSubmit = (e) => {
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...form }),
+        })
+            .then(() => alert("Success!"))
+            .catch((error) => alert(error));
+        e.preventDefault();
+    };
+    const handleChange = (event) => {
+        setForm({ ...form, [event.target.name]: event.target.value });
+    };
     return (
         <div className="formContainer">
             <div className="contactMeIntro">
                 <h1 className="title">For further informations</h1>
             </div>
             <div className="center">
-                <form name="contact" method="POST" data-netlify="true">
+                <form
+                    name="contact"
+                    onChange={handleSubmit}
+                    method="POST"
+                    data-netlify="true"
+                >
                     <input type="hidden" name="form-name" value="contact v1" />
                     <label>
                         Name:
-                        <input type="text" name="name" className="input" />
+                        <input
+                            onChange={handleChange}
+                            type="text"
+                            name="name"
+                            className="input"
+                        />
                     </label>
                     <label>
                         Email:
-                        <input type="email" name="email" className="input" />
+                        <input
+                            onChange={handleChange}
+                            type="email"
+                            name="email"
+                            className="input"
+                        />
                     </label>
                     <label>
                         Message:
-                        <textarea name="message" row="4" className="textarea" />
+                        <textarea
+                            onChange={handleChange}
+                            name="message"
+                            row="4"
+                            className="textarea"
+                        />
                     </label>
                     <button type="submit" className="button">
                         Submit
